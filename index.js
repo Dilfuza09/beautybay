b754e19f3fa1b052ec8049cb6031716586a4d947
Dilfuza <muhammedovanargiza0@gmail.com>
2025-06-19 09:55:27 +0500

    update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const url = "mongodb+srv://dilfuza:dilfuza@cluster0.y96zodz.mongodb.net/";
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: '',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
